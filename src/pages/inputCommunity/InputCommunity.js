274650cdc2f162894adf7455749f7633b839027d
cocoaq <cocoa9720@gmail.com>
2024-12-06 23:08:07 +0900

    react-quill사용
    -텍스트 에디터
    --toast-ui제거 및 react-quil도입
    --엔터가 안먹히는 사유(CSS 문제) 찾음

--- src/pages/inputCommunity/InputCommunity.js
+++ src/pages/inputCommunity/InputCommunity.js
@@ -1,35 +1,63 @@
 
-import React, { useRef } from 'react';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
+import React, { useRef, useState } from 'react';
+
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 
 function InputCommunity() {
-    const editorRef = useRef(); // 에디터 참조 생성
+    const quillRef = useRef(null);
+    const [value, setValue] = useState('');
+    const [textCount, setTextCount] = useState(0);
+    const MAX_LENGTH = 100000;
 
-    const handleGetMarkdown = () => {
-        const editorInstance = editorRef.current.getInstance(); // 에디터 인스턴스 가져오기
-        const markdown = editorInstance.getMarkdown(); // Markdown 가져오기
-        console.log(markdown); // 확인용
-
+    
+    const quillModules = {
+        toolbar: [
+            [{ header: [1, 2, false] }], 
+            ['bold', 'italic', 'underline'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ color: [] }, { background: [] }],
+            ['link'],
+            ['clean'],
+        ],
     };
+    
+
+    const handleChange = (content) => {
+        setValue(content);
+
+        const editor = quillRef.current.getEditor();
+        const text = editor.getText(); 
+        setTextCount(text.trim().length); 
+
+        if(textCount >= MAX_LENGTH){
+            alert('글자수 초과');
+        }
+
+    }
+
+
+    const testLog = () => {
+        const editor = quillRef.current.getEditor();
+        const htmlContent = editor.root.innerHTML;
+        console.log('HTML:', htmlContent);
+    }
+
 
     return (
         <div>
-            <Editor
-                ref={editorRef} // 에디터 참조 연결
-                initialValue="　"
-                previewStyle="vertical"
-                height="400px"
-                initialEditType="wysiwyg"
-                useCommandShortcut={true}
-                moveCursorToStart={true}
-                toolbarItems={[
-                    ['bold', 'italic', 'strike'], // 텍스트 스타일링
-                    ['ul', 'ol'], // 리스트 도구
-                    ['link', 'image', 'table'], // 삽입 도구
-                ]}
-                />
-            <button onClick={handleGetMarkdown}>Get Markdown</button>
+            <span>글자 수 : {textCount} (10,000자 제한)</span>
+            <ReactQuill 
+                className="editor-container"
+                
+                ref={quillRef}
+                theme="snow" 
+                value={value} 
+                onChange={handleChange}
+                modules={ quillModules } 
+            />
+            <button onClick={testLog}>확인or전송</button>
         </div>
     );
 }
